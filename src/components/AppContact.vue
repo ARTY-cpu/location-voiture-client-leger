efd8effdd8f1c26653acfc228fe75cba3968e9f1
Arthur Magnette <amagnette@magicsoftware.com>
2023-12-16 17:05:12 +0100

    contact email

--- src/components/AppContact.vue
+++ src/components/AppContact.vue
@@ -1,59 +1,86 @@
 <template>
-    <div class="hello">
-      <h1>{{ msg }}</h1>
-      <p>
-        For a guide and recipes on how to configure / customize this project,<br>
-        check out the
-        <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-      </p>
-      <h3>Installed CLI Plugins</h3>
-      <ul>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-        <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      </ul>
-      <h3>Essential Links</h3>
-      <ul>
-        <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-        <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-        <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-        <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-      </ul>
-      <h3>Ecosystem</h3>
-      <ul>
-        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-      </ul>
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-body">
+        <!-- Display a success or error message -->
+        <div v-if="message"
+          :class="{ 'alert': true, 'alert-success': isMessageSuccess, 'alert-danger': !isMessageSuccess }">
+          {{ message }}
+        </div>
+
+
+        <h2 class="card-title">Contactez-nous</h2>
+        <p class="card-text">N'hésitez pas à nous contacter pour toute question ou demande d'information.</p>
+
+        <!-- Contact Form -->
+        <form @submit.prevent="submitForm">
+          <!-- Name Input -->
+          <div class="mb-3">
+            <label for="name" class="form-label">Nom :</label>
+            <input type="text" class="form-control" id="name" v-model="formData.name" required>
+          </div>
+
+          <!-- Email Input -->
+          <div class="mb-3">
+            <label for="email" class="form-label">Email :</label>
+            <input type="email" class="form-control" id="email" v-model="formData.email" required>
+          </div>
+
+          <!-- Message Textarea -->
+          <div class="mb-3">
+            <label for="message" class="form-label">Message :</label>
+            <textarea class="form-control" id="message" rows="5" v-model="formData.message" required></textarea>
+          </div>
+
+          <!-- Submit Button -->
+          <button type="submit" class="btn btn-primary">Envoyer</button>
+        </form>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        message: '',
+      },
+    };
+  },
+  methods: {
+    async submitForm() {
+      try {
+        const response = await fetch('http://localhost:3000/contact', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(this.formData),
+        });
+
+        if (response.ok) {
+          console.log('Form data sent successfully.');
+          // Optionally, reset the form data after successful submission
+          this.formData = {
+            name: '',
+            email: '',
+            message: '',
+          };
+        } else {
+          console.error('Failed to send form data.');
+        }
+      } catch (error) {
+        console.error('Error sending form data:', error);
+      }
     }
-  }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+/* Add custom styles here if needed */
+</style>
